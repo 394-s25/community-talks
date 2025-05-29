5bebb9b6ef0b7751cc4ca2f92ded8f5e668403c6
zoryah-gray <zoryahgray2025@u.northwestern.edu>
2025-05-29 14:06:00 -0500

    fixed sizing on searchbar and spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -5,7 +5,7 @@ const LoadingSpinner = () => {
     return (
         <div style={{display:'flex', justifyContent:'center',alignItems:'center',alignContent:'center', flexDirection:'column',margin:'auto 0', color:"#007bff"}}>
             <FontAwesomeIcon icon={faSpinner} size="2x" spinPulse/>
-            <h3 style={{padding:"0.5rem"}}>Loading</h3>
+            <p style={{padding:"0.5rem"}}>Loading</p>
         </div>
     );
 }
