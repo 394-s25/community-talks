3434b9e9278641e17932a03ddaf71b9d0fcb782f
Minxin-Shi <18222930184@163.COM>
2025-06-03 15:20:40 -0500

    add logo to sider

--- src/components/SidebarNav.jsx
+++ src/components/SidebarNav.jsx
@@ -5,6 +5,16 @@ import "../css/SidebarNav.css";
 export default function SidebarNav({ sections, getIconForTitle, onSelect }) {
     return (
         <nav className="sidebar-nav">
+            <div className="sidebar-top-logo" onClick={() => navigate("/home")}>
+                <img
+                    src="/logoicon.svg"
+                    alt="Logo"
+                    width="100"
+                    height="100"
+                    className="sidebar-logo-img"
+                />
+
+            </div>
             {sections.map((section) => (
                 <a
                     key={section.title}
